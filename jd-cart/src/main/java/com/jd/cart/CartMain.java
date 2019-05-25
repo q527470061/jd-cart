72d53d5a528284dd805ff20b63bdc0c700d76f5f
qin <527470061@qq.com>
2019-05-25 13:40:29 +0800

    购物车服务

---
+++ jd-cart/src/main/java/com/jd/cart/CartMain.java
@@ -0,0 +1,13 @@
+package com.jd.cart;
+
+import org.springframework.boot.SpringApplication;
+
+import com.jd.cart.springconfig.JD_Cart_Application;
+
+
+public class CartMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JD_Cart_Application.class, args);
+	}
+}
---
+++ jd-cart/src/main/java/com/jd/cart/springconfig/SpringMVCConfig.java
@@ -0,0 +1,10 @@
+package com.jd.cart.springconfig;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringMVCConfig  {
+    
+ 
+
+}
